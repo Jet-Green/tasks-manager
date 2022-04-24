ded97d0ba458f2802b6c19083abbd1382cea75cc
Jet-Green <grishadzyin@gmail.com>
2022-04-24 09:21:46 +0400

    add vuetify and creating task with axios

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container>
+    <v-form>
+      <v-text-field v-model="task" required></v-text-field>
+      <v-btn @click="createTask">create</v-btn>
+    </v-form>
+  </v-container>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "Home",
+  data: () => ({
+    task: "",
+  }),
+  methods: {
+    createTask: async function () {
+      axios.post("http://localhost:3000/create-task", { task: this.task });
+    },
+  },
+};
+</script>
+
+<style>
+</style>
